bac7d99bcad85bc8f1d6e448efe1c94e8731c1c2
hrithikscube <hrithik@scube.me>
2024-08-05 09:22:58 +0530

    clean up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,55 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import React from 'react'
+
+const header_links = [
+    {
+        name: 'Our Hotel',
+        link: '/our-hotel'
+    },
+    {
+        name: 'Rooms & Rates',
+        link: '/rooms-and-rates'
+    },
+    {
+        name: 'Facilities',
+        link: '/facilities'
+    },
+    {
+        name: 'Contact Us',
+        link: '/contact-us'
+    },
+]
 
 // add header implementation here
 const Header = () => {
+
+    const Router = useRouter()
+
     return (
-        <div className='h-20 w-full flex items-center justify-center lg:px-10 px-5 bg-[#f2f2f2]'>Header</div>
+        <div className='w-full flex flex-col lg:gap-10 gap-6 items-center justify-center lg:px-10 px-5 bg-white lg:py-8 py-6'>
+
+            <div>
+                <Image width={151} height={101} src="/logo.svg" alt="logo" className='w-[151px] h-[101px]' />
+            </div>
+
+
+            <div className='flex items-center justify-center lg:gap-7 gap-4'>
+                {
+                    header_links.map(item => (
+                        <button onClick={() => Router.push(item.link)} className='text-[#957554] font-medium lg:text-base text-sm'>{item.name}</button>
+                    ))
+                }
+
+                <button>
+                    <Image width={16} height={16} src="/icons/search.svg" alt="search" />
+                </button>
+
+            </div>
+
+
+
+        </div>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Inter } from "next/font/google";
 import Header from '@/components/Header';
+import Filters from '@/components/Filters';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,8 @@ const Home = () => {
   return (
     <Fragment>
       <Header />
+      <Filters />
+
       <div className={`${inter.className} flex flex-col items-center justify-center w-full h-screen`}>
         <h1 className='lg:text-base text-sm'>Hotel Landing Page</h1>
       </div>
